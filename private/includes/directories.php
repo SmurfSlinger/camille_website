886520746c39876586a6778576c0c31f269359c7
SmurfSlinger <gundersen.kyler1@gmail.com>
2025-08-05 23:41:01 -0600

    started public/private dirs
    began header, footer, directories

---
+++ private/includes/directories.php
@@ -0,0 +1,7 @@
+<?php
+
+
+define('PROJECT_ROOT', dirname(__DIR__, 2));
+define('PUBLIC_PATH', PROJECT_ROOT . '/public');
+define('PRIVATE_PATH', PROJECT_ROOT . '/private');
+define('INCLUDES_PATH', PRIVATE_PATH . '/includes');
---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+require_once('../private/includes/header.php');
+?>
